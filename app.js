222ca4217889e978f87cf6c950582578aa8aafa8
WilliamTuominiemi <william.m.tuominiemi@gmail.com>
2021-02-07 15:53:16 +0200

    configured config

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require('express')
 const ejs = require('ejs')
 const paypal = require('paypal-rest-sdk')
 
+paypal.configure({
+    'mode': 'sandbox', //sandbox or live
+    'client_id': process.env.CLIENT_ID,
+    'client_secret': process.env.CLIENT_SECRET
+  });
+
 const app = express()
 
 app.set('view engine', 'ejs')
